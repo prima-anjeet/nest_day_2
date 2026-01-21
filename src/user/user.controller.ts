b8c9ecd4acee6bef7427a0d89a68b2e366aeb730
Anjeetsingh112 <anjeetsingh842310@gmail.com>
2026-01-21 13:20:39 +0530

    Implement CRUD operations on mysql database

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post,Put } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './user.entity';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService:UserService){}
+    @Get()
+    async getAllUsers():Promise<User[]> {
+        return this.userService.getAllUsers();
+    }
+    @Get(':id')
+    async getUserById(@Param('id') id:number):Promise<User>{
+        return this.userService.getUserById(id);
+    }
+    
+    
+    @Post()
+    async createUser(@Body() userData:Partial<User>): Promise<User>{
+        return this.userService.createUser(userData);
+    }
+    @Put(':id')
+    async updatedUser(@Param('id') id:number,@Body() updatedDatta:Partial<User>):Promise<User>{
+        return this.userService.updatedUser(id,updatedDatta);
+    }
+    @Delete(':id')
+    async deleteUser(@Param('id') id:number):Promise<{message:string}>{
+        return this.userService.deleteUser(id);
+    }
+    
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { Not, Repository } from 'typeorm';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(@InjectRepository(User) private userRepository: Repository<User>){}
+    //fetch all users
+    async getAllUsers(): Promise<User[]> {
+        return this.userRepository.find();
+    } 
+    //create a new user
+    async createUser(userData: Partial<User>): Promise<User>{
+        const user = this.userRepository.create(userData);
+        return this.userRepository.save(user);
+    }
+    //fetch user by id
+    async getUserById(id:number):Promise<User>{
+        const user=await this.userRepository.findOneBy({id});
+        if(!user){
+            throw new NotFoundException(`User not found`);
+        }
+        return user;
+    }
+    //update user 
+    async updatedUser(id:number, userData:Partial<User>):Promise<User>{
+        const user=await this.getUserById(id);
+        const updatedUser = Object.assign(user, userData);
+        return this.userRepository.save(updatedUser);
+    }
+  //delete user
+  async deleteUser(id:number):Promise<{message:string}>{
+    const result=await this.userRepository.delete(id);
+    if(result.affected===0){
+        throw new NotFoundException(`User not found`);
+    }
+     return { message: 'User deleted successfully' };
+  }
+}
