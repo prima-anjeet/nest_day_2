87ab7911cb5e0a0e0f7994aec6a14fbe9dfa0f29
Anjeetsingh112 <anjeetsingh842310@gmail.com>
2026-01-21 11:57:38 +0530

    Integrated TypeORM for MySQL database connection. And create user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Module, } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }),
+  TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    autoLoadEntities: true,
+    synchronize: true,
+  }),
+  UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
